46a79a7dfe94bbcac07bdce95a3c544f90fa6424
Sebastian Kann <sebastiankann@gmail.com>
2020-08-10 16:22:36 +0000

    Continuing thru the exercises now on functions

--- tinkering.js
+++ tinkering.js
@@ -1,9 +1,10 @@
-if (age<13){
-  console.log("Elementary School")
-} else if (age>= 13 && age <= 18){
-  console.log("Secondary School")
-} else {
-  console.log("Lighthouse Labs")
-}
+const amounts = [61.00, 52.25, 112.99, 5.00]; 
 
+
+let total = 0;
+for (let i of amounts) {
+  total += i;
+  console.log(i);
+}
+console.log('Order total is: ', total);
 
